3cc4ba1ef85ca2c2142dc02888b47e0416735ad6
Planteig <Planteig17@hotmail.com>
2024-04-19 15:03:06 +0200

    Created AccountService for Firebase Auth

--- app/src/main/java/com/example/eksamens_projekt/MainActivity.kt
+++ app/src/main/java/com/example/eksamens_projekt/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.eksamens_projekt.navigation.Navigation
 import com.example.eksamens_projekt.ui_elements.theme.EksamensProjektTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Navigation()
                 }
             }
         }
---
+++ app/src/main/java/com/example/eksamens_projekt/services/AccountService.kt
@@ -0,0 +1,29 @@
+package com.example.eksamens_projekt.services
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class AccountService {
+
+    fun authenticate(email: String, password: String, onResult: () -> Unit, onFail: () -> Unit) {
+        Firebase.auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+
+                //Give it the function of what to do
+                onResult()
+            }
+            .addOnFailureListener {
+                //Give it the function of what to do
+                onFail()
+            }
+    }
+
+    fun login(email: String, password: String, onResult: () -> Unit) {
+        Firebase.auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //Give it the function of what to do
+                onResult()
+            }
+
+    }
+}
